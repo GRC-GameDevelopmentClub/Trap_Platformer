6ecfb5212edfa1f06247ed4322688882c70b690e
Тимур <Child@MacBook-Pro-Timur.local>
2017-11-20 11:23:21 -0800

    PlayerMovement
    Added Script, temporary player sprite, added player prefab

---
+++ Trap_Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	private Rigidbody2D rb;
+	public float speed;
+	float moveX;
+	float moveY;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveX = Input.GetAxis ("Horizontal");
+		moveY = Input.GetAxis ("Vertical");
+		rb.velocity = new Vector2(moveX * speed,moveY * speed);
+
+	}
+}
