02ad31da469621f797ae426485edc484830d042f
Miguel Garnica <mgarnica123@aol.com>
2017-11-22 10:18:24 -0800

    Camera Follow script

---
+++ Trap_Platformer/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+    public float zoom;
+    public Vector2 offSet;
+
+    private Vector3 camPosition;
+
+	// Use this for initialization
+	void Start () {
+        camPosition = new Vector2(target.position.x, target.position.y);
+
+        this.transform.position = camPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        camPosition = this.transform.position;
+        camPosition.x += (target.position.x - this.transform.position.x) * speed;
+        camPosition.y += (target.position.y - this.transform.position.y) * speed;
+
+        this.transform.position = new Vector3(camPosition.x + offSet.x, camPosition.y + offSet.y, zoom);
+	}
+}
