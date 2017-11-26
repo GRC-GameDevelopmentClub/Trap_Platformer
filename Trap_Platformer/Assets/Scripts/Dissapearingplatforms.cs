083eb8cc535577c773910e3dd62d885d5e97a791
timurtregub <timurtregub@gmail.com>
2017-11-25 20:45:40 -0800

    Added Dissapearing platforms
    Dissapearing platforms

---
+++ Trap_Platformer/Assets/Scripts/Dissapearingplatforms.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissapearingplatforms : MonoBehaviour {
+	float time;
+	private bool IsTrapground;
+
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log (time);
+
+		if (IsTrapground) {
+			time += Time.deltaTime;
+		}
+		
+		if (IsTrapground == true && time > 0.5f) {
+			Destroy (gameObject);
+			time = 0f;
+		}
+	}
+	private void OnCollisionStay2D (Collision2D collision) {
+
+		Debug.Log ("collision");
+
+		if (collision.gameObject.CompareTag ("Player")) {
+			IsTrapground = true;
+
+		}
+	}
+}
